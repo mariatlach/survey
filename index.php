294e63d7b5842d95b39f762a987053e399164589
Maria <mariateresalachowicz@gmail.com>
2020-01-17 15:07:29 +0100

    index added

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<title>Panel użytkownika</title>
+</head>
+<body>
+</body>
+</html>
